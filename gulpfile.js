335c2bea7b7b2ba7b25c06df9292d466d3f50a4a
New <aquiplaybr@gmail.com>
2018-11-26 20:32:44 -0200

    bug ao compilar sass corrigido

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,9 @@ var gulp = require('gulp');
 var sass = require('gulp-ruby-sass');
 
 //task para chamar o sass
-gulp.task('sass', function(){
-    return sass('/sass/*.sass')
-});
+var sass = require('gulp-ruby-sass');
 
-//task default
-gulp.task('default', function(){
- 
+gulp.task('sass', function() {
+    gulp.src('sass/*.sass')
+    .pipe(gulp.dest('css'));
 });
